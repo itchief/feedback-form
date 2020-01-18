d6903e3e191cf6c123a4b5b6412193fc58bd9c66
Alexander <alex.malcev1980@gmail.com>
2020-01-18 12:31:07 +1000

    Update captcha

--- feedback/captcha/captcha.php
+++ feedback/captcha/captcha.php
@@ -11,7 +11,7 @@ session_start();
 
 $id = 'captcha';
 if (isset($_GET['id'])) {
-  $id = filter_var($_GET['id'], FILTER_SANITIZE_STRING);
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_STRING);
 }
 
 // присваиваем PHP переменной captchastring строку символов
@@ -27,7 +27,7 @@ $_SESSION[$id] = $captchaStr;
 // создаем новое изображение из файла background.png
 $image = imagecreatefrompng(dirname(__FILE__) . '/background.png');
 // устанавливаем цвет (R-200, G-240, B-240) изображению, хранящемуся в $image
-$colour = imagecolorallocate($image, 200, 240, 240);
+$colour = imagecolorallocate($image, 130, 130, 130);
 // присваиваем переменной font название шрифта
 $font = dirname(__FILE__) . '/oswald.ttf';
 // устанавливаем случайное число между -10 и 10 градусов для поворота текста
@@ -36,7 +36,7 @@ $rotate = rand(-10, 10);
 // 2 - размер шрифта (18), 3 - угол поворота текста ($rotate),
 // 4, 5 - начальные координаты x и y для текста (18,30), 6 - индекс цвета ($colour),
 // 7 - путь к файлу шрифта ($font), 8 - текст ($captchaStr)
-imagettftext($image, 18, $rotate, 28, 32, $colour, $font, $captchaStr);
+imagettftext($image, 36, $rotate, 56, 64, $colour, $font, $captchaStr);
 // будем передавать изображение в формате png
 header('Content-type: image/png');
 //выводим изображение
